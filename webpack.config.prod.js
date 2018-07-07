f03e61845f659692eee024272bbf99dbb0b2392c
nathan m weller <nkas17@gmail.com>
2018-07-07 14:23:54 -0400

    fix prod build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,29 +3,25 @@
  *
  * @see - https://webpack.js.org/guides/hmr-react/#webpack-config
  */
-const { resolve, join } = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const { resolve } = require('path');
 const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
+	mode: 'production',
 	resolve: {
-		modules: [join(__dirname, 'src'), 'node_modules'],
 		extensions: ['.js', '.jsx', '.json', '.css', '.scss'],
+		alias: {},
 	},
-
-	entry: './index.jsx',
-
+	entry: [
+		// the entry point of our app
+		'./index.jsx',
+	],
 	output: {
-		// the output bundle
-		filename: 'bundle.js',
-
 		path: resolve(__dirname, 'dist'),
 	},
 
 	context: resolve(__dirname, 'src'),
-
-	devtool: 'source-map',
 
 	module: {
 		rules: [
@@ -36,15 +32,12 @@ module.exports = {
 			},
 			{
 				test: /\.css$/,
-				use: ExtractTextPlugin.extract({
-					fallback: 'style-loader',
-					use: ['css-loader'],
-				}),
+				use: ['style-loader', 'css-loader'],
 			},
-			{
-				test: /\.json$/,
-				loader: 'json-loader',
-			},
+			// {
+			// 	test: /\.json$/,
+			// 	loader: 'json-loader',
+			// },
 			{
 				test: /\.(jpe?g|png|gif|ico)$/i,
 				loader: 'file-loader?name=[name].[ext]',
@@ -68,36 +61,15 @@ module.exports = {
 		],
 	},
 	plugins: [
-		// optimizes order
-		new ExtractTextPlugin('styles.css'),
+		// enable HMR globally
+		new webpack.HotModuleReplacementPlugin(),
 
-		// // Moves the index.html file over and asset folder to the dist folder
+		// Moves the index.html file over and asset folder to the dist folder
 		new CopyWebpackPlugin([
-			// {output}/dist/file.txt
 			{ from: 'index.html' },
 
-			// 	// Copy directory contents to {output}/to/directory/
-			// 	{ from: 'assets', to: 'assets' },
+			// Copy directory contents to {output}/to/directory/
+			{ from: 'assets', to: 'assets' },
 		]),
-		new webpack.LoaderOptionsPlugin({
-			minimize: true,
-			debug: false,
-		}),
-		new webpack.DefinePlugin({
-			'process.env': {
-				NODE_ENV: JSON.stringify('production'),
-			},
-		}),
-		new webpack.optimize.UglifyJsPlugin({
-			beautify: false,
-			mangle: {
-				screw_ie8: true,
-				keep_fnames: true,
-			},
-			compress: {
-				screw_ie8: true,
-			},
-			comments: false,
-		}),
 	],
 };
